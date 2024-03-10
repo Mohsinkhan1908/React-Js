298ecd243088cd3048b76000ee5c55a3a1ea30bb
Mohsinkhan1908 <khanmohsin1908@gmail.com>
2024-03-10 21:24:14 +0530

    "saved"

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./contexts/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState('light')
+
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+
+  useEffect(()=>{
+    const html = document.querySelector('html');
+    html.classList.remove('light','dark')
+    html.classList.add(themeMode)
+  },[themeMode])
 
   return (
-    <>
-      <h1 className='text-3xl bg-pink-800 p-4'>Chai Aur Code</h1>
-    </>
-  )
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          <ThemeBtn />
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          <Card />
+        </div>
+      </div>
+    </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
